4f45dab92c3ec461f353d5da7f39d666c7d3f0a1
Alex <Alex@ALEX-PC>
2016-11-06 13:58:00 +0000

    commit

---
+++ assignment attempt 2/assignment attempt 2/ClubModels/CubContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace assignment_attempt_2.ClubModels
+{
+    public class CubContext
+    {
+        public DbSet<Club> Clubs { get; set; }
+
+        public DbSet<Member> Members { get; set; }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<ClubEvent> ClubEvents { get; set; }
+    }
+}
---
+++ assignment attempt 2/assignment attempt 2/ClubModels/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace assignment_attempt_2.ClubModels
+{
+    [Table("Student")]
+    public class Student
+    {
+        public string StudentID { get; set; }
+
+        public string Fname { get; set; }
+
+        public string Sname { get; set; }
+    }
+}
+ 
